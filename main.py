1c12835c8fe18a0c9d0fa7d6dd800cbc0fd56b43
Jesús Benavides <jesus.benavides2888@alumnos.udg.mx>
2025-10-21 23:20:52 -0600

    fix: version tracking

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from github_updater import GitHubUpdater
 
 class CriptasApp:
     # Versión de la aplicación (actualizar en cada release)
-    VERSION = "1.1.0"
+    VERSION = "1.1.1"
 
     def __init__(self):
         self.root = tk.Tk()
